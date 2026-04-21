40c483ae60de7d712025a918d137a2d91e70a15c
Qi Bin Lei <qlei@attsub01.usatlas.bnl.gov>
2026-04-20 20:00:16 -0400

    suppress fastframes error

--- NTuple_Hist/fastframes/BNL/run_fastframes.sh
+++ NTuple_Hist/fastframes/BNL/run_fastframes.sh
@@ -13,6 +13,7 @@ echo "::endgroup::"
 
 date -u "+%Y-%m-%dT%H:%M:%SZ" >> split.log
 
+# shellcheck disable=SC1091
 source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -m /atlasgpfs01/usatlas/data/ -c el9 -r "asetup StatAnalysis,0.6.2 &&\
   source /atlasgpfs01/usatlas/data/qlei/FastFramesTutorial/TutorialClass/build/setup.sh &&\
   python3 /atlasgpfs01/usatlas/data/qlei/FastFramesTutorial/FastFrames/python/FastFrames.py -c /atlasgpfs01/usatlas/data/qlei/input/mc20e_example_config.yml 2>&1 | tee fastframes.log"
