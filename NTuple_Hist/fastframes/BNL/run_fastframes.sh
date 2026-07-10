744222bb494df05ff2fe195c42c603b1b5a8c13d
Qi Bin Lei <qlei@attsub01.usatlas.bnl.gov>
2026-07-10 14:58:28 -0400

    Fix sourced benchmark bug, changed permissions for BNL cron scripts to executable, and updated cron script for BNL

--- NTuple_Hist/fastframes/BNL/run_fastframes.sh
+++ NTuple_Hist/fastframes/BNL/run_fastframes.sh
@@ -4,7 +4,6 @@
 source ./parsing/utils/benchmark_utils.sh
 
 # current time used for log file storage
-
 start_time=$(date -u "+%Y-%m-%dT%H:%M:%SZ")
 
 cd /atlasgpfs01/usatlas/data/qlei/ || exit
@@ -20,7 +19,7 @@ echo "::endgroup::"
 date -u "+%Y-%m-%dT%H:%M:%SZ" >> split.log
 
 # shellcheck disable=SC1091
-source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -m /atlasgpfs01/usatlas/data/ -c el9 -r "asetup StatAnalysis,0.6.2 &&\
+source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -m /atlasgpfs01/usatlas/data/ -c el9 -r "asetup StatAnalysis,0.6.3 &&\
   source /atlasgpfs01/usatlas/data/qlei/FastFramesTutorial/TutorialClass/build/setup.sh &&\
   echo \"SETUP_COMPLETE=\$(date -u '+%Y-%m-%dT%H:%M:%SZ')\" >> split.log &&\
   /usr/bin/time -v python3 /atlasgpfs01/usatlas/data/qlei/FastFramesTutorial/FastFrames/python/FastFrames.py -c /atlasgpfs01/usatlas/data/qlei/input/mc20e_example_config.yml 2>&1 | tee fastframes.log"
@@ -46,3 +45,5 @@ append_benchmark fastframes.log "${start_time}" "${end_time}" "${setup_start}" "
 
 # Moves log to outputdir
 mv fastframes.log "${output_dir}"
+mv split.log "${output_dir}"
+
